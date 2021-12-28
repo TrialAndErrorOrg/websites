a7c8cb82435f71255cd2ee553e3b4d3cc5ac8056
Thomas F. K. Jorna <hello@tefkah.com>
2021-12-28 23:31:57 +0100

    fix: db config

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,23 +1,18 @@
-const path = require("path");
-
+const parse = require("pg-connection-string").parse;
+const config = parse(process.env.DATABASE_URL);
 module.exports = ({ env }) => ({
   connection: {
-    client: "postgresql",
+    client: "postgres",
     connection: {
-      filename: path.join(
-        __dirname,
-        "..",
-        env("DATABASE_FILENAME", ".tmp/data.db")
-      ),
-      host: env("SUPABASE_URL", "127.0.0.1"),
-      port: env.int("DATABASE_PORT", 3306),
-      database: env("DATABASE_NAME", "strapi"),
-      user: env("DATABASE_USERNAME", "strapi"),
-      password: env("SUPABASE_SECRET", "strapi"),
+      host: config.host,
+      port: config.port,
+      database: config.database,
+      user: config.user,
+      password: config.password,
       ssl: {
-        rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false), // For self-signed certificates
+        rejectUnauthorized: false,
       },
     },
-    //useNullAsDefault: true,
+    debug: false,
   },
 });
