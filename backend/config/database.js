0f0f651ab9520ebc8f8c4889e2b0c2f218f33c4c
Thomas F. K. Jorna <hello@tefkah.com>
2021-12-29 17:54:30 +0100

    aaa

--- backend/config/database.js
+++ backend/config/database.js
@@ -7,7 +7,7 @@ module.exports = ({ env }) => ({
         client: 'postgres',
         host: env('DATABASE_HOST', '127.0.0.1'),
         port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'strapi'),
+        database: env('DATABASE_NAME', 'support'),
         username: env('DATABASE_USERNAME', ''),
         password: env('DATABASE_PASSWORD', ''),
       },
