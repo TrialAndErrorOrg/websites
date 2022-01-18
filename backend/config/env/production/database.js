559342f153dc018a0b7d8027b120dca1e4c635e5
Thomas F. K. Jorna <hello@tefkah.com>
2022-01-18 21:25:17 +0100

    fix(cd): use env instead of file for SSL cert

--- backend/config/env/production/database.js
+++ backend/config/env/production/database.js
@@ -1,19 +1,22 @@
-const path = require('path');
-const fs = require('fs');
+const path = require("path");
+const fs = require("fs");
 
 module.exports = ({ env }) => ({
   connection: {
-    client: 'postgres',
+    client: "postgres",
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'support'),
-      user: env('DATABASE_USERNAME', 'support'),
-      password: env('DATABASE_PASSWORD', ''),
-      schema: env('DATABASE_SCHEMA', 'public'), // Not required
+      host: env("DATABASE_HOST", "127.0.0.1"),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "support"),
+      user: env("DATABASE_USERNAME", "support"),
+      password: env("DATABASE_PASSWORD", ""),
+      schema: env("DATABASE_SCHEMA", "public"), // Not required
       ssl: {
-	      ca: env('NODE_ENV') === 'production' ? fs.readFileSync(env('CERT_PATH', './cert')).toString() : undefined,
-        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+        ca:
+          env("NODE_ENV") === "production"
+            ? Buffer.from(env("CERT", ""), "base64").toString("utf-8")
+            : undefined,
+        rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false), // For self-signed certificates
       },
     },
     debug: false,
