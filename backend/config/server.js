7cd621f5f23f8192cdbce1fca169d52195fda14e
Thomas F. K. Jorna <hello@tefkah.com>
2022-01-19 11:05:57 +0100

    fix: add proxy settings?

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,4 +1,6 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', 'localhost'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "localhost"),
+  port: env.int("PORT", 1337),
+  url: "https://admin.centeroftrialanderror.com/",
+  proxy: true,
 });
