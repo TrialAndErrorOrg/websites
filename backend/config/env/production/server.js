fe6cb24aff619aea786ad563f8553856e6a94377
Thomas F. K. Jorna <hello@tefkah.com>
2022-03-31 13:35:32 +0200

    fix: properly initialize app keys for session middleware

--- backend/config/env/production/server.js
+++ backend/config/env/production/server.js
@@ -9,7 +9,7 @@ module.exports = ({ env }) => ({
     secret: env("ADMIN_JWT_SECRET", "c0b47f9208b27587591171747a858bc8"),
   },
   app: {
-    keys: env.array("APP_KEYS", env("STRAPI_APP_KEYS", ["secret", "key"])),
+    keys: env.array("STRAPI_APP_KEYS"),
   },
   cron: {
     enabled: true,
--- backend/config/server.js
+++ backend/config/server.js
@@ -1,9 +1,10 @@
 const cronTasks = require("@webbio/strapi-plugin-scheduler/cron-task");
+
 module.exports = ({ env }) => ({
   host: env("HOST", "localhost"),
   port: env.int("PORT", 1337),
   app: {
-    keys: env.array("APP_KEYS", env("STRAPI_APP_KEYS", ["secret", "key"])),
+    keys: env.array("STRAPI_APP_KEYS"),
   },
   cron: {
     enabled: true,
