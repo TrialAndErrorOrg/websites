8158bfc42049ead218a53bc0036b92e74085c91c
Thomas F. K. Jorna <hello@tefkah.com>
2022-05-03 20:54:19 +0200

    fix: try to make bundle smaller

--- backend/config/env/production/database.js
+++ backend/config/env/production/database.js
@@ -7,8 +7,8 @@ module.exports = ({ env }) => ({
     connection: {
       host: env("DATABASE_HOST", "127.0.0.1"),
       port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "support"),
-      user: env("DATABASE_USERNAME", "support"),
+      database: env("DATABASE_NAME", "strapi"),
+      user: env("DATABASE_USERNAME", "strapi"),
       password: env("DATABASE_PASSWORD", ""),
       schema: env("DATABASE_SCHEMA", "public"), // Not required
       ssl: {
--- backend/config/env/production/server.js
+++ backend/config/env/production/server.js
@@ -2,10 +2,10 @@ const cronTasks = require("@webbio/strapi-plugin-scheduler/cron-task");
 module.exports = ({ env }) => ({
   host: env("HOST", "localhost"),
   port: env.int("PORT", 1337),
-  url: "https://admin.centeroftrialanderror.com/",
+  url: `${env("PROD_HOST", "https://admin.centeroftrialanderror.com")}`,
   proxy: true,
   admin: {
-    url: "https://admin.centeroftrialanderror.com/admin",
+    url: `${env("PROD_HOST", "https://admin.centeroftrialanderror.com")}/admin`,
     secret: env("ADMIN_JWT_SECRET", "c0b47f9208b27587591171747a858bc8"),
   },
   app: {
