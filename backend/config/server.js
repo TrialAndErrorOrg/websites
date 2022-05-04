ef83c7f8fa21859316afa6313701997cdbfb0e4f
Thomas F. K. Jorna <hello@tefkah.com>
2022-05-04 15:50:25 +0200

    fix: set host to 0.0.0.0

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,7 +1,7 @@
 const cronTasks = require("@webbio/strapi-plugin-scheduler/cron-task");
 
 module.exports = ({ env }) => ({
-  host: env("HOST", "localhost"),
+  host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
   app: {
     keys: env.array("STRAPI_APP_KEYS"),
