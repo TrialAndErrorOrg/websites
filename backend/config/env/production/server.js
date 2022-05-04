2de5dbbb108465dad62cea5dd0edb0962d5e87ff
Thomas F. K. Jorna <hello@tefkah.com>
2022-05-04 16:40:31 +0200

    fix: cms link bad can't do env vars apparently

--- backend/config/env/production/server.js
+++ backend/config/env/production/server.js
@@ -2,10 +2,10 @@ const cronTasks = require("@webbio/strapi-plugin-scheduler/cron-task");
 module.exports = ({ env }) => ({
   host: env("HOST", "localhost"),
   port: env.int("PORT", 8080),
-  url: `${env("PROD_HOST", "https://admin.centeroftrialanderror.com")}`,
+  url: `${env("PROD_HOST", "https://cms.centeroftrialanderror.com")}`,
   proxy: true,
   admin: {
-    url: `${env("PROD_HOST", "https://admin.centeroftrialanderror.com")}/admin`,
+    url: `${env("PROD_HOST", "https://cms.centeroftrialanderror.com")}/admin`,
     secret: env("ADMIN_JWT_SECRET", "c0b47f9208b27587591171747a858bc8"),
   },
   app: {
