8bff28fa5cfa0e73921b008902c4f1f0c8bc192b
Thomas F. K. Jorna <hello@tefkah.com>
2022-05-04 15:23:59 +0200

    fix: set port to 8080

--- backend/config/env/production/server.js
+++ backend/config/env/production/server.js
@@ -1,7 +1,7 @@
 const cronTasks = require("@webbio/strapi-plugin-scheduler/cron-task");
 module.exports = ({ env }) => ({
   host: env("HOST", "localhost"),
-  port: env.int("PORT", 1337),
+  port: env.int("PORT", 8080),
   url: `${env("PROD_HOST", "https://admin.centeroftrialanderror.com")}`,
   proxy: true,
   admin: {
