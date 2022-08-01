5c15519a2f46c69c40ae8c5be164c6e5915ec465
Thomas F. K. Jorna <hello@tefkah.com>
2022-08-01 12:18:28 +0200

    change base url to trialanderror.org

--- apps/strapi/config/env/production/server.ts
+++ apps/strapi/config/env/production/server.ts
@@ -3,10 +3,10 @@ import cronTasks from '@webbio/strapi-plugin-scheduler/cron-task'
 export default ({ env }) => ({
   host: env('HOST', 'localhost'),
   port: env.int('PORT', 8080),
-  url: `${env('PROD_HOST', 'https://cms.centeroftrialanderror.com')}`,
+  url: `${env('PROD_HOST', 'https://cms.trialanderror.org')}`,
   proxy: true,
   admin: {
-    url: `${env('PROD_HOST', 'https://cms.centeroftrialanderror.com')}/admin`,
+    url: `${env('PROD_HOST', 'https://cms.trialanderror.org')}/admin`,
     secret: env('ADMIN_JWT_SECRET', 'c0b47f9208b27587591171747a858bc8'),
   },
   app: {
