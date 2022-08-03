7610730208d951a45834759b5fd919109752d35e
Thomas F. K. Jorna <hello@tefkah.com>
2022-08-03 19:12:42 +0200

    create next-auth-orcid-provider

---
+++ libs/next-auth-orcid-provider/src/lib/next-auth-orcid-provider.ts
@@ -0,0 +1,28 @@
+import { OAuthUserConfig, OAuthConfig } from 'next-auth/providers'
+
+export interface OrcidProfile extends Record<string, any> {
+  family_name: string
+  given_name: string
+  name: string
+  auth_time: string
+  iss: string
+  sub: string
+}
+
+export function OrcidProvider<P extends OrcidProfile>(options: OAuthUserConfig<P>): OAuthConfig<P> {
+  return {
+    id: 'orcid',
+    name: 'ORCID',
+    type: 'oauth',
+    wellKnown: 'https://orcid.org/.well-known/openid-configuration',
+    authorization: { params: { scope: 'openid' } },
+    idToken: true,
+    checks: ['pkce', 'state'],
+    profile(profile) {
+      return {
+        id: profile.sub,
+        name: profile.name,
+      }
+    },
+  }
+}
