3d84a40e27507dd343d45ac70d5a4b7c99ee111b
Thomas F. K. Jorna <hello@tefkah.com>
2022-10-03 11:16:44 +0200

    add blog template

--- apps/blog/astro.config.mjs
+++ apps/blog/astro.config.mjs
@@ -1,10 +1,45 @@
-import { defineConfig } from 'astro/config'
-import preact from '@astrojs/preact'
-import sitemap from '@astrojs/sitemap'
-import tailwind from '@astrojs/tailwind'
-import turbolinks from '@astrojs/turbolinks'
+import path from 'path';
+import { fileURLToPath } from 'url';
 
+import { defineConfig } from 'astro/config';
+
+import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
+import image from '@astrojs/image';
+import partytown from '@astrojs/partytown';
+
+import { SITE } from './src/config.mjs';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+// https://astro.build/config
 export default defineConfig({
-  outDir: '../../dist/apps/blog',
-  integrations: [preact(), sitemap(), tailwind(), turbolinks()],
-})
+	// Astro uses this full URL to generate your sitemap and canonical URLs in your final build
+	site: SITE.origin,
+	base: SITE.basePathname,
+
+	output: 'static',
+
+	integrations: [
+		tailwind({
+			config: {
+				applyBaseStyles: false,
+			},
+		}),
+		sitemap(),
+		image(),
+
+		/* Disable this integration if you don't use Google Analytics (or other external script). */
+		partytown({
+			config: { forward: ['dataLayer.push'] },
+		}),
+	],
+
+	vite: {
+		resolve: {
+			alias: {
+				'~': path.resolve(__dirname, './src'),
+			},
+		},
+	},
+});
