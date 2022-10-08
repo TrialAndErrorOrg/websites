f7ddae757a68f56950d4bfc2aceeb970ef3ab77f
Thomas F. K. Jorna <hello@tefkah.com>
2022-10-08 17:59:57 +0200

    add boness

--- apps/positions/tailwind.config.cjs
+++ apps/positions/tailwind.config.cjs
@@ -7,48 +7,23 @@ const { join } = require('path')
  * @type {import('tailwindcss').Config}
  */
 module.exports = {
-	presets: [require('../../tailwind-workspace-preset.js')],
-	content: [
-		join(__dirname, 'src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}'),
+  presets: [require('../../tailwind-workspace-preset.js')],
+  content: [
+    join(__dirname, 'src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}'),
 
-		...createGlobPatternsForDependencies(__dirname),
-	],
-	theme: {
-		extend: {
-			colors: {
-				primary: colors.blue,
-				secondary: colors.pink,
-			},
-			fontFamily: {
-				sans: ["'Open Sans'", ...defaultTheme.fontFamily.sans],
-			},
-		},
-	},
-	plugins: [require('@tailwindcss/typography')],
-	darkMode: 'class',
+    ...createGlobPatternsForDependencies(__dirname),
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: colors.blue,
+        secondary: colors.pink,
+      },
+      fontFamily: {
+        sans: ["'Open Sans'", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [require('@tailwindcss/typography')],
+  darkMode: 'class',
 }
-
-/* 
-
-  Alternative tailwind.config.js
-  
-  NOTE: Add this fonts to <head>
-    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;500;700&display=swap" rel="stylesheet" />
-*/
-
-// module.exports = {
-//   content: ["./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}"],
-//   theme: {
-//     extend: {
-//       colors: {
-//         primary: colors.cyan,
-//         secondary: colors.lime,
-//       },
-//       fontFamily: {
-//         sans: ["'Nunito'", ...defaultTheme.fontFamily.sans],
-//       },
-//     },
-//   },
-//   plugins: [require("@tailwindcss/typography")],
-//   darkMode: "class",
-// };
