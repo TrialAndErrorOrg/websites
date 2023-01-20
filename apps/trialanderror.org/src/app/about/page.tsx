3c9d26297d0ffc11379ebb2488380922b8e12e0e
Thomas F. K. Jorna <hello@tefkah.com>
2023-01-20 16:28:13 +0100

    slightly better

--- apps/trialanderror.org/src/app/about/page.tsx
+++ apps/trialanderror.org/src/app/about/page.tsx
@@ -15,14 +15,16 @@ export default async function AboutPage() {
             <h1 className="-mt-10 text-4xl font-bold text-blue-500 md:-mt-16 md:text-7xl">
               About Us
             </h1>
-            <p className="relative flex w-[70vw] items-center leading-[1.4] text-blue-500 before:absolute before:-ml-4 before:hidden before:h-1/2 before:w-2 before:bg-blue-500 md:w-auto md:text-lg md:before:-ml-12 md:before:flex md:before:w-3 lg:ml-40 2xl:text-[2rem]">
+            <p className="relative flex w-[70vw] items-center leading-[1.4] text-blue-500 before:absolute before:-ml-4 before:hidden before:h-1/2 before:w-2 before:bg-blue-500 md:w-auto md:text-lg md:before:-ml-12 md:before:flex md:before:w-3 lg:ml-40 2xl:text-[2rem] 2xl:leading-10">
               The Center of Trial & Error is a virtual platform and sandbox for disruptive
-              initiatives that will lead to institutional change. With the Journal of Trial & Error
-              as our flagship, we aim to bridge the gap between what researchers do and what
-              researchers can report, to discuss and question what kind of knowledge is deemed
-              valuable, to advocate for practices that positively impact the lives of researchers
-              and the public, and to advance policies that put openness, transparency and compassion
-              above profit, division and bottom lines.
+              initiatives that will lead to institutional change.
+              <br />
+              <br />
+              With the Journal of Trial & Error as our flagship, we aim to bridge the gap between
+              what researchers do and what researchers can report, to discuss and question what kind
+              of knowledge is deemed valuable, to advocate for practices that positively impact the
+              lives of researchers and the public, and to advance policies that put openness,
+              transparency and compassion above profit, division and bottom lines.
             </p>
           </div>
         </div>
