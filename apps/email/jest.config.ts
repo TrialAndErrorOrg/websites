19d17815a7155dce2984a1c4c05543fc8ea6e33a
Thomas F. K. Jorna <hello@tefkah.com>
2023-02-05 16:25:43 +0100

    fix: add emails so that ci works

---
+++ apps/email/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'trialanderror.org',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nrwl/next/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/trialanderror.org',
+}
