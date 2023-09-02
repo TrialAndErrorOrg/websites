e62a297828c1cf2230fcfffac65ba401a2dd0681
Thomas F. K. Jorna <thomas@tefkah.kpn>
2023-09-02 21:10:52 +0200

    fix: use correct types

--- apps/strapi/src/api/collaborator/routes/collaborator.ts
+++ apps/strapi/src/api/collaborator/routes/collaborator.ts
@@ -2,6 +2,6 @@
  * collaborator router
  */
 
-import { factories } from '@strapi/strapi';
+import { factories } from '@strapi/strapi'
 
-export default factories.createCoreRouter('api::collaborator.collaborator');
+export default factories.createCoreRouter('api::collaborator.collaborator')
