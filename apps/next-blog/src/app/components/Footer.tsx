27895e0110585c0702b7623aa8d57ad7d16eb286
Thomas F. K. Jorna <hello@tefkah.com>
2024-03-20 14:33:23 +0100

    feat: trigger deploy

--- apps/next-blog/src/app/components/Footer.tsx
+++ apps/next-blog/src/app/components/Footer.tsx
@@ -26,40 +26,37 @@ export async function Footer() {
       <div className="mx-auto px-4 py-12 sm:px-6 md:max-w-7xl lg:px-8 lg:py-16 xl:max-w-[100rem]">
         <div className="pb-8 xl:grid xl:grid-cols-5 xl:gap-8">
           <div className="grid grid-cols-2 flex-wrap gap-4 md:flex md:gap-8 xl:col-span-4">
-            {
-              // TODO: [blog] clean up the footer
-              new Array(Math.floor(((footer?.length ?? 0) - 0.1) / 2) + 1).fill(0).map((_, idx) => (
-                <div
-                  key={_ + idx}
-                  className={`flex-wrap md:flex md:grid-cols-${
-                    Math.floor(((footer?.length ?? 0) - 0.1) / 2) + 1
-                  } md:gap-8`}
-                >
-                  {[footer?.[idx * 2], footer?.[idx * 2 + 1]]?.map((section, ix) => (
-                    <div
-                      className={ix % 2 ? 'mt-12 md:mt-0' : ''}
-                      key={section?.title ?? section?.url ?? section?.createdAt?.toString() ?? ix}
-                    >
-                      <h3 className="text-sm font-semibold uppercase tracking-wider text-white transition-colors hover:text-orange-500 dark:text-slate-200">
-                        {section?.url ? <a href={section.url}>{section?.title}</a> : section?.title}
-                      </h3>
-                      <ul className="mt-4 space-y-4">
-                        {section?.children?.map((item, indx) => (
-                          <li key={item.title}>
-                            <a
-                              href={item.url ?? '/'}
-                              className="sleek-underline text-base text-white dark:text-slate-100 dark:hover:text-white"
-                            >
-                              {item.title}
-                            </a>
-                          </li>
-                        ))}
-                      </ul>
-                    </div>
-                  ))}
-                </div>
-              ))
-            }
+            {new Array(Math.floor(((footer?.length ?? 0) - 0.1) / 2) + 1).fill(0).map((_, idx) => (
+              <div
+                key={_ + idx}
+                className={`flex-wrap md:flex md:grid-cols-${
+                  Math.floor(((footer?.length ?? 0) - 0.1) / 2) + 1
+                } md:gap-8`}
+              >
+                {[footer?.[idx * 2], footer?.[idx * 2 + 1]]?.map((section, ix) => (
+                  <div
+                    className={ix % 2 ? 'mt-12 md:mt-0' : ''}
+                    key={section?.title ?? section?.url ?? section?.createdAt?.toString() ?? ix}
+                  >
+                    <h3 className="text-sm font-semibold uppercase tracking-wider text-white transition-colors hover:text-orange-500 dark:text-slate-200">
+                      {section?.url ? <a href={section.url}>{section?.title}</a> : section?.title}
+                    </h3>
+                    <ul className="mt-4 space-y-4">
+                      {section?.children?.map((item, indx) => (
+                        <li key={item.title}>
+                          <a
+                            href={item.url ?? '/'}
+                            className="sleek-underline text-base text-white dark:text-slate-100 dark:hover:text-white"
+                          >
+                            {item.title}
+                          </a>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                ))}
+              </div>
+            ))}
           </div>
         </div>
         <div className="lg:items-top flex flex-col gap-4 border-t border-white pt-8 dark:border-white lg:flex-row lg:justify-between xl:mt-0">
