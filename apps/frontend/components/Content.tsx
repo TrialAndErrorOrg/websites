2e471dd8cf43badfad3cacdca61bfb6fe9aa4d8f
Thomas F. K. Jorna <hello@tefkah.com>
2024-03-20 14:53:03 +0100

    chore: remove old unushed stuff

--- apps/frontend/components/Content.tsx
+++
@@ -1,6 +0,0 @@
-export const Content = ({ children }: { children: string }) => (
-  <div
-    className="prose prose-slate dark:prose-invert"
-    dangerouslySetInnerHTML={{ __html: children }}
-  />
-)
