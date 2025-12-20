4e550d93adaf47fbc1bb7eca01da4ac37c49ac05
Thomas F. K. Jorna <hello@tefkah.com>
2025-12-20 19:17:05 +0100

    fix: add analytics

--- apps/next-blog/src/app/layout.tsx
+++ apps/next-blog/src/app/layout.tsx
@@ -1,54 +1,70 @@
-import { AnalyticsWrapper } from './components/Analytics'
-import { Footer } from './components/Footer'
-import { createMetadata } from '../utils/createMetadata'
+import { Open_Sans, Overpass } from "next/font/google";
+import Script from "next/script";
+import { createMetadata } from "../utils/createMetadata";
+import { AnalyticsWrapper } from "./components/Analytics";
+import { Footer } from "./components/Footer";
 
-import { Overpass, Open_Sans } from 'next/font/google'
-import Script from 'next/script'
-
-import '../styles/globals.css'
-import { HeaderWrapper } from './components/HeaderWrapper'
+import "../styles/globals.css";
+import { HeaderWrapper } from "./components/HeaderWrapper";
 
 const overpass = Overpass({
-  subsets: ['latin'],
-  variable: '--font-overpass',
-  display: 'swap',
-})
+	subsets: ["latin"],
+	variable: "--font-overpass",
+	display: "swap",
+});
 
 const open_sans = Open_Sans({
-  subsets: ['latin'],
-  variable: '--font-open-sans',
-  display: 'swap',
-})
+	subsets: ["latin"],
+	variable: "--font-open-sans",
+	display: "swap",
+});
 
-export const metadata = createMetadata({})
+export const metadata = createMetadata({});
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en" className={`${open_sans.variable} ${overpass.variable}`}>
-      <head>
-        <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="manifest" href="/site.webmanifest" />
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#002642" />
-        <meta name="msapplication-TileColor" content="#002642" />
-        <meta name="theme-color" content="#ffffff" />
-      </head>
+export default function RootLayout({
+	children,
+}: {
+	children: React.ReactNode;
+}) {
+	return (
+		<html lang="en" className={`${open_sans.variable} ${overpass.variable}`}>
+			<head>
+				<meta charSet="UTF-8" />
+				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+				<link
+					rel="apple-touch-icon"
+					sizes="180x180"
+					href="/apple-touch-icon.png"
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="32x32"
+					href="/favicon-32x32.png"
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="16x16"
+					href="/favicon-16x16.png"
+				/>
+				<link rel="manifest" href="/site.webmanifest" />
+				<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#002642" />
+				<meta name="msapplication-TileColor" content="#002642" />
+				<meta name="theme-color" content="#ffffff" />
+			</head>
 
-      <body className="selection:bg-orange-200/50 flex min-h-screen flex-col bg-white tracking-tight text-gray-900 antialiased transition-colors selection:border selection:border-black selection:transition-all dark:bg-slate-800 dark:text-slate-300">
-        <HeaderWrapper />
-        <div className="flex-grow">{children}</div>
-        <Footer />
-        <AnalyticsWrapper />
-        <Script
-          strategy="afterInteractive"
-          data-domain="blog.trialanderror.org"
-          src="/stats/js/script"
-          data-api="/stats/api/event"
-        />
-      </body>
-    </html>
-  )
+			<body className="selection:bg-orange-200/50 flex min-h-screen flex-col bg-white tracking-tight text-gray-900 antialiased transition-colors selection:border selection:border-black selection:transition-all dark:bg-slate-800 dark:text-slate-300">
+				<HeaderWrapper />
+				<div className="flex-grow">{children}</div>
+				<Footer />
+				<AnalyticsWrapper />
+				<Script
+					defer
+					src="https://analytics.trialanderror.org/script.js"
+					data-website-id="24642014-38d2-478a-9bb0-35e9f29746ea"
+				/>
+			</body>
+		</html>
+	);
 }
--- apps/trialanderror.org/src/app/layout.tsx
+++ apps/trialanderror.org/src/app/layout.tsx
@@ -1,48 +1,73 @@
-import { Overpass, Open_Sans } from 'next/font/google'
-import { AnalyticsWrapper } from './components/Analytics'
-import { Footer } from './components/Footer'
-import { Nav } from './components/Nav'
-import { createMetadata } from '../utils/createMetadata'
+import { Open_Sans, Overpass } from "next/font/google";
+import { createMetadata } from "../utils/createMetadata";
+import { AnalyticsWrapper } from "./components/Analytics";
+import { Footer } from "./components/Footer";
+import { Nav } from "./components/Nav";
 
-import '../styles/globals.css'
+import "../styles/globals.css";
+import Script from "next/script";
 
 const overpass = Overpass({
-  subsets: ['latin'],
-  variable: '--font-overpass',
-  // display: 'swap',
-})
+	subsets: ["latin"],
+	variable: "--font-overpass",
+	// display: 'swap',
+});
 
 const openSans = Open_Sans({
-  subsets: ['latin'],
-  variable: '--font-open-sans',
-  // display: 'swap',
-})
+	subsets: ["latin"],
+	variable: "--font-open-sans",
+	// display: 'swap',
+});
 
 export const metadata = createMetadata({
-  title: 'Center of Trial and Error',
-  description: 'Creating transparent and responsible scholarship.',
-})
+	title: "Center of Trial and Error",
+	description: "Creating transparent and responsible scholarship.",
+});
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en" className={`${openSans.variable} ${overpass.variable}`}>
-      <head>
-        <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="manifest" href="/site.webmanifest" />
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#002642" />
-        <meta name="msapplication-TileColor" content="#002642" />
-        <meta name="theme-color" content="#ffffff" />
-      </head>
-      <body>
-        <Nav />
-        <main>{children}</main>
-        <Footer />
-        <AnalyticsWrapper />
-      </body>
-    </html>
-  )
+export default function RootLayout({
+	children,
+}: {
+	children: React.ReactNode;
+}) {
+	return (
+		<html lang="en" className={`${openSans.variable} ${overpass.variable}`}>
+			<head>
+				<meta charSet="UTF-8" />
+				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+				<link
+					rel="apple-touch-icon"
+					sizes="180x180"
+					href="/apple-touch-icon.png"
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="32x32"
+					href="/favicon-32x32.png"
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="16x16"
+					href="/favicon-16x16.png"
+				/>
+				<link rel="manifest" href="/site.webmanifest" />
+				<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#002642" />
+				<meta name="msapplication-TileColor" content="#002642" />
+				<meta name="theme-color" content="#ffffff" />
+			</head>
+			<body>
+				<Nav />
+				<main>{children}</main>
+				<Footer />
+				<AnalyticsWrapper />
+
+				<Script
+					defer
+					src="https://analytics.trialanderror.org/script.js"
+					data-website-id="60d1573a-ec6c-43e7-88cb-2fdaae0701b1"
+				/>
+			</body>
+		</html>
+	);
 }
