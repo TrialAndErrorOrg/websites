e1b609dd65ce5ce790248ce9b7226e4ba8a0bdd9
Thomas F. K. Jorna <hello@tefkah.com>
2026-02-08 22:56:26 +0100

    fix: actually dogshit stupid should not be allowed near a computer

--- apps/strapi/config/env/production/database.ts
+++ apps/strapi/config/env/production/database.ts
@@ -1,24 +1,27 @@
-import { Env } from '../../admin'
+import type { Env } from "../../admin";
 
 export default ({ env }: { env: Env }) => ({
-  connection: {
-    client: 'postgres',
-    connection: {
-      client: 'postgres',
-      host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'strapi'),
-      user: env('DATABASE_USERNAME', 'strapi'),
-      password: env('DATABASE_PASSWORD', ''),
-      // schema: env('DATABASE_SCHEMA', 'public'), // Not required
-      ssl: {
-        ca:
-          env('NODE_ENV') === 'production'
-            ? Buffer.from(env('CERT', ''), 'base64').toString('utf-8')
-            : undefined,
-        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
-      },
-    },
-    debug: false,
-  },
-})
+	connection: {
+		client: "postgres",
+		connection: {
+			client: "postgres",
+			host: env("DATABASE_HOST", "127.0.0.1"),
+			port: env.int("DATABASE_PORT", 5432),
+			database: env("DATABASE_NAME", "strapi"),
+			user: env("DATABASE_USERNAME", "strapi"),
+			password: env("DATABASE_PASSWORD", ""),
+			// schema: env('DATABASE_SCHEMA', 'public'), // Not required
+			ssl: {
+				...(!env.bool("SSL", false)
+					? { ssl: false }
+					: {
+							ssl: {
+								ca: Buffer.from(env("CERT", ""), "base64").toString("utf-8"),
+								rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false), // For self-signed certificates
+							},
+						}),
+			},
+		},
+		debug: false,
+	},
+});
