6aa9a9e08141860480b3f9d543b628dd2fc4d704
Thomas F. K. Jorna <hello@tefkah.com>
2026-06-05 13:54:03 +0200

    chore: remove peer-review app

--- apps/peer-review/app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Page() {
-  return (
-    <div className="container">
-      <main>hey</main>
-    </div>
-  )
-}
--- apps/peer-review/utils/asyncComponentHack.ts
+++
@@ -1,4 +0,0 @@
-// TODO: Remove this once either TypeScript or Next.js fixes this issue
-export function asyncComponentHack<T, R>(fn: (arg: T) => Promise<R>): (arg: T) => R {
-  return fn as (arg: T) => R
-}
