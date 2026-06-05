4ef719b9efaba62d798574b93d73b5694334dd21
chartgerink <chris@libscie.org>
2026-06-05 13:38:37 +0200

    Remove placeholder contact page (#151)

--- apps/trialanderror.org/src/app/contact/pageX.tsx
+++
@@ -1,7 +0,0 @@
-export default async function ContactPage() {
-  return (
-    <div className="flex h-screen w-full items-center justify-center">
-      <p>Nothing to see here yet!</p>
-    </div>
-  )
-}
